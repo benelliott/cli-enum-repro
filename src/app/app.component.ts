ffcc20294e8a4ca9f56eb1d8032f737df0bf1a60
Ben Elliott <ben.elliott@gsacapital.com>
2017-09-08 15:21:47 +0100

    Add repro for enum issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Enum } from '../enums/enum';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  enum = Enum.Foo;
+
+
+  public ngOnInit(): void {
+    this.enum = Enum.Bar;
+    console.log(this.enum);
+  }
 }
---
+++ src/enums/enum.ts
@@ -0,0 +1,4 @@
+export enum Enum {
+  Foo,
+  Bar
+}
